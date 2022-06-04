ac1de628f43b2e410d344e57657842c928198204
sfilaktos@skill90.io <sfilaktos@skill90.io>
2022-06-04 19:56:08 +0300

    More on aws

--- aws_api/Program.cs
+++ aws_api/Program.cs
@@ -1,13 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.UseSwagger();
 app.UseSwaggerUI();
+
 
 app.Run();
