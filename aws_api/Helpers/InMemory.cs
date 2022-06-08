87afe8feda37b2bf0d278cb8dde314bd54ad3afc
sfilaktos@skill90.io <sfilaktos@skill90.io>
2022-06-09 00:44:50 +0300

    test rabbitmq..

--- aws_api/Helpers/InMemory.cs
+++ aws_api/Helpers/InMemory.cs
@@ -3,4 +3,5 @@ namespace aws_api.Helpers;
 public static class InMemory
 {
     public static int IntForCache { get; set; } = 0;
+    public static string Message { get; set; } = string.Empty;
 }
--- aws_api/Program.cs
+++ aws_api/Program.cs
@@ -1,4 +1,6 @@
+using System.Text;
 using aws_api.Helpers;
+using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
@@ -7,11 +9,39 @@ builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 
 var app = builder.Build();
 
+
+
+const string url = "amqps://fylstathis:fylstathis21@b-dcce8793-99e9-464d-9a53-93e5c11a2f37.mq.us-east-1.amazonaws.com:5671";
+const string queueName = "test-queue";
+
+var model = new {Name = "John Doe"};
+var serializedModel = JsonConvert.SerializeObject(model);
+var bytes = Encoding.UTF8.GetBytes(serializedModel);
+
+var connectionFactory = new RabbitMQ.Client.ConnectionFactory()
+{
+    Uri=  new Uri(url)
+};
+var connection = connectionFactory.CreateConnection();
+var channel = connection.CreateModel();
+channel.QueueDeclare(queueName, false, true, false, null);
+channel.BasicPublish("", queueName, false, null, bytes);
+
+var consumer = new RabbitMQ.Client.Events.EventingBasicConsumer(channel);
+consumer.Received += (sender, e) =>
+{
+    var stringMessage = Encoding.UTF8.GetString(e.Body.ToArray());
+    InMemory.Message = stringMessage;
+    Console.WriteLine(stringMessage);
+};
+
+channel.BasicConsume(queueName, true, "", true, true, null, consumer);
+
 app.MapGet("/", () =>
 {
-    InMemory.IntForCache += 1;
-    return InMemory.IntForCache;
+    return InMemory.Message;
 });
+
 app.UseSwagger();
 app.UseSwaggerUI();
 app.Run();
